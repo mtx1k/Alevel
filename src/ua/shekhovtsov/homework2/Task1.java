d97ba315d29833237ddbdac3e3615e5deaf8b8b6
mtx1k <30780721+mtx1k@users.noreply.github.com>
2022-01-26 19:47:25 +0200

    change method name to calcTriangleAreaByCoordinate

--- src/ua/shekhovtsov/homework2/Task1.java
+++ src/ua/shekhovtsov/homework2/Task1.java
@@ -8,9 +8,9 @@ package ua.shekhovtsov.homework2;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println(Task1.triangleAreaByCoordinate(1, 3, 2, -5, -8, 4));
+        System.out.println(Task1.calcTriangleAreaByCoordinate(1, 3, 2, -5, -8, 4));
     }
-    public static double triangleAreaByCoordinate(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public static double calcTriangleAreaByCoordinate(int x1, int y1, int x2, int y2, int x3, int y3) {
         return 0.5 * Math.abs(((x1 - x3) * (y2 - y3)) - ((x2 - x3) * (y1 - y3)));
     }
 }
